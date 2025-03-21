b958be753824360c63376243926e398897dcc334
Nikhil0723 <nikhilvishwakarma0723@gmail.com>
2025-03-21 09:06:22 +0530

    initial

--- dist/service-worker.js
+++
@@ -1,7 +0,0 @@
-importScripts("/precache-manifest.69f4191103dd9c1c82eeb790d28152f9.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
-
-workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
-
-workbox.skipWaiting();
-workbox.clientsClaim();
-
--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,33 @@
-import React, { useState } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { useState } from "react";
+import { Switch, Route } from "react-router-dom";
 
-import Header from './header';
-import Main from './routes/Main';
-import Tab from './tab';
-import Auth from './authentication/auth/Auth';
-import Title from './Title/Title';
+import Header from "./header";
+import Main from "./routes/Main";
+import Tab from "./tab";
+import Auth from "./authentication/auth/Auth";
+import Title from "./Title/Title";
 
 const App = () => {
-  const [loggedIn, setLoggedIn] = useState(localStorage.getItem('token') ? true : false);
+  const [loggedIn, setLoggedIn] = useState(true);
 
   return (
     <>
-      {loggedIn ?
-        <div className='main__wrapper '>
+      {loggedIn ? (
+        <div className="main__wrapper ">
           <Header />
           <Title />
           <Tab />
           <Main />
         </div>
-
-        :
-        <div className='main__wrapper'>
+      ) : (
+        <div className="main__wrapper">
           <Switch>
-            <Route exact path='/' component={Auth} />
+            <Route exact path="/" component={Auth} />
           </Switch>
         </div>
-      }
+      )}
     </>
-  )
+  );
 };
 
 export default App;
